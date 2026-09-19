a15804beb390dbef8caf527b4c7a556a38f71321
Elamin75 <amtaam@gmail.com>
2026-09-19 14:47:22 +0200

    Added GET Shifts API endpoint and Database Seeder

--- Backend/ClinicManagement.Api/Controllers/ShiftsController.cs
+++ Backend/ClinicManagement.Api/Controllers/ShiftsController.cs
@@ -11,10 +11,27 @@ namespace ClinicManagement.Api.Controllers
     public class ShiftsController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly ClinicManagement.Application.Common.Interfaces.IApplicationDbContext _context;
 
-        public ShiftsController(IMediator mediator)
+        public ShiftsController(IMediator mediator, ClinicManagement.Application.Common.Interfaces.IApplicationDbContext context)
         {
             _mediator = mediator;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAvailableShifts([FromQuery] Guid doctorId, [FromQuery] DateTime date)
+        {
+            // For testing, just return all active shifts
+            var shifts = System.Linq.Enumerable.Select(_context.DoctorShifts, s => new
+            {
+                ShiftId = s.Id,
+                ShiftName = s.ShiftName,
+                StartTime = s.StartTime.ToString(@"hh\:mm"),
+                EndTime = s.EndTime.ToString(@"hh\:mm"),
+                CurrentQueueSize = s.NextAvailableQueueNumber - 1
+            });
+            return Ok(shifts);
         }
 
         [HttpPost("{id}/call-next")]
